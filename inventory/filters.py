e2fd9b93225167a3d6737f340193c9def4c14d72
moubhub <moibhub@gmail.com>
2025-08-20 01:14:56 +0400

    add filters with ordering & search on /api/inventory-levels/

--- inventory/filters.py
+++ inventory/filters.py
@@ -7,13 +7,14 @@ from .models import InventoryItem
 class InventoryItemFilter(django_filters.FilterSet):
     min_price = django_filters.NumberFilter(field_name='price', lookup_expr='gte') # Praice >= min_praice
     max_price = django_filters.NumberFilter(field_name='price', lookup_expr='lte') # Praice <= max_praice
+    
     category = django_filters.CharFilter(field_name='category', lookup_expr='iexact') # exact category match
     low_stock = django_filters.NumberFilter(method='filter_low_stock') # custom filter for items with low quantity
 
 
     class Meta:
         model = InventoryItem
-        fields = ['category']
+        fields = ['category', 'min_price', 'max_price', 'low_stock']
 
     # custom filter method
     # returns items with quantity less than - equal to the given threshold
