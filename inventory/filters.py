b39b24108a8ac31e28066ac0cf1b5b92718841fb
moubhub <moibhub@gmail.com>
2025-08-20 01:41:31 +0400

    add flexible low_stock filter with default and user-defined threshold

--- inventory/filters.py
+++ inventory/filters.py
@@ -9,19 +9,24 @@ class InventoryItemFilter(django_filters.FilterSet):
     max_price = django_filters.NumberFilter(field_name='price', lookup_expr='lte') # Praice <= max_praice
     
     category = django_filters.CharFilter(field_name='category', lookup_expr='iexact') # exact category match
-    low_stock = django_filters.NumberFilter(method='filter_low_stock') # custom filter for items with low quantity
+    # Custom boolean filter for low stock items
+    # ﻻy default, returns items with quantity less than 5
+    # هf the user provides a custom threshold (low_stock_threshold), use that instead
+    low_stock = django_filters.BooleanFilter(method='filter_low_stock')
 
 
     class Meta:
         model = InventoryItem
         fields = ['category', 'min_price', 'max_price', 'low_stock']
 
-    # custom filter method
-    # returns items with quantity less than - equal to the given threshold
-    # defaults to 5 if value is missing - invalid
+    
     def filter_low_stock(self, queryset, name, value):
-        try:
-            threshold = int(value)
-        except (TypeError, ValueError):
-            threshold = 5
-        return queryset.filter(quantity__lte=threshold)
+        if value:  # filter only if user activated the boolean filter
+            try:
+                # try to get the user-defined threshold from query params
+                threshold = int(self.request.query_params.get('low_stock_threshold', 5))
+            except (TypeError, ValueError):
+                # fallback to default threshold if invalid input
+                threshold = 5
+            return queryset.filter(quantity__lt=threshold)  # return items below the threshold
+        return queryset  # return all items if filter not activated
